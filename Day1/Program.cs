4f10cc41be91f94a8a7efb316999c6f456b73655
aklyy <62080538+aklyy@users.noreply.github.com>
2025-01-22 02:26:24 +0100

    initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+List<Int32> firstList = new List<Int32>();
+List<Int32> secondList = new List<Int32>();
+
+foreach (string line in input)
+{
+    string[] tokens = line.Split();
+    firstList.Add(Int32.Parse(tokens[0]));
+    secondList.Add(Int32.Parse(tokens[3]));
+}
+
+firstList.Sort();
+secondList.Sort();
+
+Int32 totalDistance = 0;
+for (int i = 0; i < firstList.Count; i++)
+{
+    totalDistance += Math.Abs(firstList[i] - secondList[i]);
+}
+
+Console.WriteLine("Part 1: " + totalDistance);
+
+int simillarityScore = firstList
+    .Sum(first => first * secondList.Count(second => second == first));
+
+Console.WriteLine("Part 2: " + simillarityScore);
+
+
+
