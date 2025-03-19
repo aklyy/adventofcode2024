6090deaf2eaeaf03f6a1ceb574acff5e813ca1fd
aklyy <62080538+aklyy@users.noreply.github.com>
2025-03-19 18:13:54 +0100

    Full day 3 regex solution.

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,15 +2,50 @@
 
 string input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
 
-string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+string mulPattern = @"mul\((\d{1,3}),(\d{1,3})\)";
 
-MatchCollection matches = Regex.Matches(input, pattern);
+MatchCollection matches = Regex.Matches(input, mulPattern);
 int result = 0;
 
 foreach (Match match in matches)
 {
-    Console.WriteLine(match.Value);
     result += Convert.ToInt32(match.Groups[1].Value) * Convert.ToInt32(match.Groups[2].Value);
+}
+
+Console.WriteLine(result);
+
+string enhancedMulPattern = @"(mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\))";
+matches = Regex.Matches(input, enhancedMulPattern);
+bool enableMul = true;
+result = 0;
+
+foreach (Match match in matches)
+{
+    string token = match.Value;
+
+    if(token == "don't()")
+    {
+        enableMul = false;
+    }
+
+    else if(token == "do()")
+    {
+        enableMul = true;
+    }
+
+    else if(enableMul && token.StartsWith("mul("))
+    {
+        Match numbersMatch = Regex.Match(token, mulPattern);
+
+        if (numbersMatch.Success)
+        {
+            int num1 = int.Parse(numbersMatch.Groups[1].Value);
+            int num2 = int.Parse(numbersMatch.Groups[2].Value);
+            int product = num1 * num2;
+
+            result += product;
+        }
+    }
 }
 
 Console.WriteLine(result);
